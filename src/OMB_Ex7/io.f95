bf32ce6a374a201414cd5d1c821171087d6f2a8e
James Munroe <jmunroe@mun.ca>
2021-03-09 20:48:37 -0500

    modify OMBEx7 to be a short wave

--- src/OMB_Ex7/io.f95
+++ src/OMB_Ex7/io.f95
@@ -10,8 +10,9 @@ CONTAINS
         CHARACTER(LEN=50), INTENT(IN) :: outfile
 
         INTEGER :: ncid, x_dimid, z_dimid, time_dimid
-        INTEGER :: time_id
-        INTEGER :: h_id, hzero_id
+        INTEGER :: time_id, x_id, z_id
+        INTEGER :: h_id
+        INTEGER :: dp_id
         INTEGER :: u_id, eta_id
         INTEGER, DIMENSION(3) :: dimids
 
@@ -28,17 +29,17 @@ CONTAINS
         CALL check(nf90_put_att(ncid, time_id, "units", "seconds since 2000-01-01"))
         CALL check(nf90_put_att(ncid, time_id, "calendar", "gregorian"))
 
-        !CALL check(nf90_def_var(ncid, "x", NF90_FLOAT, x_dimid, x_varid))
-        !CALL check(nf90_put_att(ncid, x_varid, "units", "m"))
-        !CALL check(nf90_def_var(ncid, "z", NF90_FLOAT, z_dimid, z_varid))
-        !CALL check(nf90_put_att(ncid, z_varid, "units", "m"))
+        CALL check(nf90_def_var(ncid, "x", NF90_FLOAT, x_dimid, x_id))
+        CALL check(nf90_put_att(ncid, x_id, "units", "m"))
+        CALL check(nf90_def_var(ncid, "z", NF90_FLOAT, z_dimid, z_id))
+        CALL check(nf90_put_att(ncid, z_id, "units", "m"))
 
         dimids = (/z_dimid, x_dimid, time_dimid/)
 
         !define variables: from name, type, dims
-        CALL check(nf90_def_var(ncid, "hzero", NF90_FLOAT, (/z_dimid, x_dimid/), hzero_id))
         CALL check(nf90_def_var(ncid, "h", NF90_FLOAT, dimids, h_id))
         CALL check(nf90_def_var(ncid, "u", NF90_FLOAT, dimids, u_id))
+        CALL check(nf90_def_var(ncid, "dp", NF90_FLOAT, dimids, dp_id))
         CALL check(nf90_def_var(ncid, "eta", NF90_FLOAT, dimids, eta_id))
 
         !nf90_put_att  ! assign attribute values
@@ -46,6 +47,8 @@ CONTAINS
         CALL check(nf90_put_att(ncid, h_id, "description", "interval thickness"))
         CALL check(nf90_put_att(ncid, u_id, "units", "m s-1"))
         CALL check(nf90_put_att(ncid, u_id, "description", "velocity"))
+        CALL check(nf90_put_att(ncid, dp_id, "units", "Pa"))
+        CALL check(nf90_put_att(ncid, dp_id, "description", "dynamic pressure"))
         CALL check(nf90_put_att(ncid, eta_id, "units", "m"))
         CALL check(nf90_put_att(ncid, eta_id, "description", "displacement"))
 
@@ -53,7 +56,8 @@ CONTAINS
         CALL check(nf90_enddef(ncid))
 
         !Write Data
-        CALL check(nf90_put_var(ncid, hzero_id, hzero(:, 1:nx)))
+        CALL check(nf90_put_var(ncid, x_id, x(1:nx)))
+        CALL check(nf90_put_var(ncid, z_id, z(1:nz)))
 
         ! close: save new netcdf dataset
         CALL check(nf90_close(ncid))
@@ -67,7 +71,7 @@ CONTAINS
         CHARACTER(LEN=50), INTENT(IN) :: outfile
         REAL, INTENT(IN) :: time
         INTEGER :: ncid, time_dimid
-        INTEGER :: u_id, eta_id, h_id, time_id
+        INTEGER :: dp_id, u_id, eta_id, h_id, time_id
         INTEGER :: n
 
         !Open the netCDF file for writing
@@ -88,6 +92,9 @@ CONTAINS
         CALL check(nf90_inq_varid(ncid, "u", u_id))
         CALL check(nf90_put_var(ncid, u_id, u(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
 
+        CALL check(nf90_inq_varid(ncid, "dp", dp_id))
+        CALL check(nf90_put_var(ncid, dp_id, dp(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
         CALL check(nf90_inq_varid(ncid, "eta", eta_id))
         CALL check(nf90_put_var(ncid, eta_id, eta(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
 
@@ -96,6 +103,7 @@ CONTAINS
 
         ! close the netCDF file
         CALL check(nf90_close(ncid))
+
     END SUBROUTINE write_nc
 !:=========================================================================t
 
