a09581cd2f3db8ca3d09b71eeff4f797334373cb
James Munroe <jmunroe@mun.ca>
2021-03-29 01:01:38 -0400

    lecture 16

---
+++ src/AOM_Ex16/main.f95
@@ -0,0 +1,83 @@
+!======================================
+! Exercise 16: Geostrophic adjustment
+!======================================
+! Author: J. Kaempf, August 2009
+
+PROGRAM slice
+
+    USE param
+    USE sub
+    USE io
+
+! local parameters
+    INTEGER :: ntot, nout
+    CHARACTER(LEN=50) :: outfile
+
+!**********
+    CALL INIT  ! initialisation
+!**********
+
+! runtime parameters
+    ntot = INT(60.*60*60./dt)
+    time = 0.0
+
+! output frequency
+    nout = INT(60.*60./dt)
+
+! open output files
+    OPEN (10, file='q.dat', form='formatted')
+    OPEN (20, file='u.dat', form='formatted')
+    OPEN (25, file='v.dat', form='formatted')
+    OPEN (30, file='w.dat', form='formatted')
+    OPEN (40, file='eta.dat', form='formatted')
+    OPEN (50, file='rho.dat', form='formatted')
+
+    
+    ! create output file
+    outfile = 'output.nc'
+    CALL create_nc(outfile)
+
+    ! write out initial conditions
+    CALL write_nc(outfile, 0.)
+
+!---------------------------
+! simulation loop
+!---------------------------
+    DO n = 1, ntot
+
+        time = time + dt
+        write (6, *) "time (hours)", time/(3600.)
+
+        ad = MIN(time/(6.*3600.0), 1.0) ! adjustment parameter
+
+        IF (ad < 1.0) THEN
+
+            DO i = 1, 25
+            DO k = 51 - 10, 51 + 10
+                IF (wet(i, k)) rho(i, k) = rho(i, k) - dt*0.1/(6*3600.)
+            END DO
+            END DO
+
+        END IF
+
+! prognostic equations
+        CALL dyn
+
+! data output
+        IF (MOD(n, nout) == 0) THEN
+            CALL write_nc(outfile, time)
+
+            DO i = 1, nz
+                WRITE (10, '(101F12.6)') (q(i, k)/(RHOREF*G), k=1, nx)
+                WRITE (20, '(101F12.6)') (u(i, k), k=1, nx)
+                WRITE (25, '(101F12.6)') (v(i, k), k=1, nx)
+                WRITE (30, '(101F12.6)') (w(i, k), k=1, nx)
+                WRITE (50, '(101F12.6)') (rho(i, k), k=1, nx)
+            END DO
+            WRITE (40, '(101F12.6)') (q(0, k)/(RHOREF*G), k=1, nx)
+            WRITE (6, *) "Data output at time = ", time/(24.*3600.)
+        END IF
+
+    END DO ! end of iteration loop
+
+END PROGRAM slice
---
+++ src/AOM_Ex17/io.f95
@@ -0,0 +1,144 @@
+MODULE io
+    USE param
+
+CONTAINS
+
+!=======================
+    SUBROUTINE create_nc(outfile)
+        USE netcdf
+        IMPLICIT NONE
+        CHARACTER(LEN=50), INTENT(IN) :: outfile
+
+        INTEGER :: ncid, x_dimid, z_dimid, time_dimid
+        INTEGER :: time_id, x_id, z_id
+        INTEGER :: p_id, q_id, rho_id
+        INTEGER :: u_id, v_id, w_id
+        INTEGER :: c1_id, c2_id, c3_id
+        INTEGER, DIMENSION(3) :: dimids
+
+        !create netCDF dataset: enter define mode
+        CALL check(nf90_create(outfile, NF90_CLOBBER, ncid))
+
+        ! define dimensions: from name and length
+        CALL check(nf90_def_dim(ncid, "time", NF90_UNLIMITED, time_dimid))
+        CALL check(nf90_def_dim(ncid, "x", nx, x_dimid))
+        CALL check(nf90_def_dim(ncid, "z", nz, z_dimid))
+
+        !Define coordinate variables
+        CALL check(nf90_def_var(ncid, "time", NF90_FLOAT, time_dimid, time_id))
+        CALL check(nf90_put_att(ncid, time_id, "units", "seconds since 2000-01-01"))
+        CALL check(nf90_put_att(ncid, time_id, "calendar", "gregorian"))
+
+        CALL check(nf90_def_var(ncid, "x", NF90_FLOAT, x_dimid, x_id))
+        CALL check(nf90_put_att(ncid, x_id, "units", "m"))
+        CALL check(nf90_def_var(ncid, "z", NF90_FLOAT, z_dimid, z_id))
+        CALL check(nf90_put_att(ncid, z_id, "units", "m"))
+
+        dimids = (/z_dimid, x_dimid, time_dimid/)
+
+        !define variables: from name, type, dims
+        CALL check(nf90_def_var(ncid, "u", NF90_FLOAT, dimids, u_id))
+        CALL check(nf90_def_var(ncid, "v", NF90_FLOAT, dimids, v_id))
+        CALL check(nf90_def_var(ncid, "w", NF90_FLOAT, dimids, w_id))
+        CALL check(nf90_def_var(ncid, "p", NF90_FLOAT, dimids, p_id))
+        CALL check(nf90_def_var(ncid, "q", NF90_FLOAT, dimids, q_id))
+        CALL check(nf90_def_var(ncid, "rho", NF90_FLOAT, dimids, rho_id))
+        CALL check(nf90_def_var(ncid, "c1", NF90_FLOAT, dimids, c1_id))
+        CALL check(nf90_def_var(ncid, "c2", NF90_FLOAT, dimids, c2_id))
+        CALL check(nf90_def_var(ncid, "c3", NF90_FLOAT, dimids, c3_id))
+
+        !nf90_put_att  ! assign attribute values
+        CALL check(nf90_put_att(ncid, u_id, "units", "m s-1"))
+        CALL check(nf90_put_att(ncid, u_id, "description", "velocity"))
+        CALL check(nf90_put_att(ncid, v_id, "units", "m s-1"))
+        CALL check(nf90_put_att(ncid, v_id, "description", "velocity"))
+        CALL check(nf90_put_att(ncid, w_id, "units", "m s-1"))
+        CALL check(nf90_put_att(ncid, w_id, "description", "velocity"))
+        CALL check(nf90_put_att(ncid, p_id, "units", "Pa"))
+        CALL check(nf90_put_att(ncid, p_id, "description", "hydrostatic pressure"))
+        CALL check(nf90_put_att(ncid, q_id, "units", "Pa"))
+        CALL check(nf90_put_att(ncid, q_id, "description", "nonhydrostatic pressure"))
+        CALL check(nf90_put_att(ncid, rho_id, "units", "kg m-3"))
+        CALL check(nf90_put_att(ncid, rho_id, "description", "density"))
+
+        !end definitions: leave define mode
+        CALL check(nf90_enddef(ncid))
+
+        !Write Data
+        CALL check(nf90_put_var(ncid, x_id, x(1:nx)))
+        CALL check(nf90_put_var(ncid, z_id, z(1:nz)))
+
+        ! close: save new netcdf dataset
+        CALL check(nf90_close(ncid))
+
+    END SUBROUTINE create_nc
+
+!:=========================================================================
+    SUBROUTINE write_nc(outfile, time)
+        USE netcdf
+        IMPLICIT NONE
+        CHARACTER(LEN=50), INTENT(IN) :: outfile
+        REAL, INTENT(IN) :: time
+        INTEGER :: ncid, time_dimid
+        INTEGER :: p_id, q_id, u_id, v_id, w_id, time_id, rho_id
+        INTEGER :: c1_id, c2_id, c3_id 
+        INTEGER :: n
+
+        !Open the netCDF file for writing
+        CALL check(nf90_open(outfile, NF90_WRITE, ncid))
+
+        ! Get ID of unlimited (time) dimension
+        CALL check(nf90_inquire(ncid, unlimiteddimid=time_dimid))
+
+        ! How many time records are there so far?
+        CALL check(nf90_inquire_dimension(ncid, time_dimid, len=n))
+
+        !Look up the variable id
+        CALL check(nf90_inq_varid(ncid, "u", u_id))
+        CALL check(nf90_put_var(ncid, u_id, u(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "v", v_id))
+        CALL check(nf90_put_var(ncid, v_id, v(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "w", w_id))
+        CALL check(nf90_put_var(ncid, w_id, w(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "p", p_id))
+        CALL check(nf90_put_var(ncid, p_id, p(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "q", q_id))
+        CALL check(nf90_put_var(ncid, q_id, q(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "rho", rho_id))
+        CALL check(nf90_put_var(ncid, rho_id, rho(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "c1", c1_id))
+        CALL check(nf90_put_var(ncid, c1_id, c1(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "c2", c2_id))
+        CALL check(nf90_put_var(ncid, c2_id, c2(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "c3", c3_id))
+        CALL check(nf90_put_var(ncid, c3_id, c3(1:nz, 1:nx), start=(/1, 1, n + 1/), count=(/nz, nx, 1/)))
+
+        CALL check(nf90_inq_varid(ncid, "time", time_id))
+        CALL check(nf90_put_var(ncid, time_id, time, start=(/n + 1/)))
+
+        ! close the netCDF file
+        CALL check(nf90_close(ncid))
+    END SUBROUTINE write_nc
+!:=========================================================================t
+
+!Check (ever so slightly modified from www.unidata.ucar.edu)
+!:======================================================================
+    SUBROUTINE check(istatus)
+        USE netcdf
+        IMPLICIT NONE
+        INTEGER, INTENT(IN) :: istatus
+        IF (istatus /= nf90_noerr) THEN
+            write (*, *) TRIM(ADJUSTL(nf90_strerror(istatus)))
+        END IF
+    END SUBROUTINE check
+!:======================================================================
+
+END MODULE io
